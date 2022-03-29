909de9760cdb1348a4cf34eb9b9d9dd9aa9b0e4c
Alfredo Medina C <medina8911@gmail.com>
2022-03-29 00:29:38 -0600

    Uploading files with multer

---
+++ controllers/storage.js
@@ -0,0 +1,27 @@
+const { storageModel } = require('../models')
+const PUBLIC_URL = process.env.PUBLIC_URL;
+
+const getItems = async (req, res) => {
+    const data = await storageModel.find({});
+    res.send({data})
+};
+const getItem = (req, res) => {};
+
+const createItem = async (req, res) => {
+    const { body, file } = req
+    console.log(file)
+    const fileData = {
+        filename: file.filename,
+        url:`${PUBLIC_URL}/${file.filename}`
+    }
+    const data = await storageModel.create(fileData)
+    res.send({data})
+};
+
+const updateItem = (req, res) => {};
+
+const deleteItem = (req, res) => {};
+
+
+
+module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
---
+++ routes/storage.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const uploadMiddleware = require('../utils/handleStorage');
+const { createItem } = require('../controllers/storage');
+
+
+router.post('/', uploadMiddleware.single('myfile'), createItem)
+
+module.exports = router;
