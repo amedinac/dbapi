312750c6b1b1a6ea5fc6ebce96817b735eced2ac
Alfredo Medina C <medina8911@gmail.com>
2022-03-30 01:27:36 -0600

    validations of data POST

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getItems, createItem } =require('../controllers/tracks'); 
+const { validatorCreateItem } = require('../validators/tracks');
+const { getItems, createItem } = require('../controllers/tracks'); 
 
 router.get('/', getItems);
-router.post('/', createItem);
+router.post('/', validatorCreateItem, createItem);
 
 module.exports = router;
