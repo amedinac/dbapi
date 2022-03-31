b3bd88d0682670e59d2f568e3de9bbb588cf5cf1
Alfredo Medina C <medina8911@gmail.com>
2022-03-30 23:19:43 -0600

    Handle Errors

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,16 +1,30 @@
-const {tracksModel} = require('../models')
+const { matchedData } = require('express-validator');
+const {tracksModel} = require('../models');
+const { handleHttpError } = require('../utils/handleError');
 
 const getItems = async (req, res) => {
-    const data = await tracksModel.find({});
-    res.send({data})
+
+    try{
+        const data = await tracksModel.find({});
+        res.send({data})
+    }catch(e){
+        handleHttpError(res, 'ERROR_GET_ITEMS')
+    }
+
+    
 };
 const getItem = (req, res) => {};
 
 const createItem = async (req, res) => {
-    const { body } = req
-    console.log(body)
-    const data = await tracksModel.create(body)
-    res.send({data})
+
+    try{
+        const body = matchedData(req)
+        const data = await tracksModel.create(body)
+        res.send({data})
+    }catch(e){
+        handleHttpError(res, 'ERROR_CREATE_ITEMS')
+    }
+
 };
 
 const updateItem = (req, res) => {};
--- routes/tracks.js
+++ routes/tracks.js
@@ -5,6 +5,6 @@ const { validatorCreateItem } = require('../validators/tracks');
 const { getItems, createItem } = require('../controllers/tracks'); 
 
 router.get('/', getItems);
-router.post('/', validatorCreateItem, customHeader, createItem);
+router.post('/', validatorCreateItem, createItem);
 
 module.exports = router;
