38059e5c1c2ecf05c8ae7d7c3477b30741147ea1
Alfredo Medina C <medina8911@gmail.com>
2022-04-01 15:26:08 -0600

    Finish storage controllers

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,27 +1,64 @@
 const { storageModel } = require('../models')
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
+const { handleHttpError } = require('../utils/handleError');
+const { matchedData } = require('express-validator');
+const fs = require('fs');
+
 
 const getItems = async (req, res) => {
-    const data = await storageModel.find({});
-    res.send({data})
+    try{
+        const data = await storageModel.find({});
+        res.send({data})
+    }catch(e){
+        handleHttpError(res, 'ERROR_GET_ITEMS');
+    }
 };
-const getItem = (req, res) => {};
+const getItem = async (req, res) => {
+    try{
+        const { id } = matchedData(req);
+        const data = await storageModel.findById(id);
+        res.send({data})
+    }catch(e){
+        handleHttpError(res, 'ERROR_GET_ITEM');
+    }
+};
 
 const createItem = async (req, res) => {
-    const { body, file } = req
-    console.log(file)
-    const fileData = {
+
+    try{
+        const { file } = req
+        const fileData = {
         filename: file.filename,
         url:`${PUBLIC_URL}/${file.filename}`
-    }
-    const data = await storageModel.create(fileData)
-    res.send({data})
+        }
+        const data = await storageModel.create(fileData)
+        res.send({data})
+        }catch(e){
+        handleHttpError(res, 'ERROR_CREATE_ITEM');
+        }
 };
 
-const updateItem = (req, res) => {};
 
-const deleteItem = (req, res) => {};
+const deleteItem = async (req, res) => {
+    try{
+        const { id } = matchedData(req);
+        const dataFile = await storageModel.findById(id);
+        await storageModel.delete({_id:id});
+        const { filename } = dataFile;
+        const filePath = `${MEDIA_PATH}/${filename}`;
+
+        //fs.unlinkSync(filePath); Delete file in local storage
+        const data = {
+            filePath,
+            deleted:1
+        }
+
+        res.send({data});
+    }catch(e){
+        handleHttpError(res, 'ERROR_DELETE_ITEM');
+    }
+}; 
 
 
-
-module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
+module.exports = { getItems, getItem, createItem, deleteItem };
--- routes/storage.js
+++ routes/storage.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const uploadMiddleware = require('../utils/handleStorage');
-const { createItem } = require('../controllers/storage');
+const { getItems, getItem, updateItem, deleteItem ,createItem } = require('../controllers/storage');
+const { validatorGetItem } = require('../validators/storage');
 
 
+//List Items
+router.get('/', getItems)
+//Detail Item
+router.get('/:id', validatorGetItem, getItem)
+//Delete Item
+router.delete('/:id', validatorGetItem, deleteItem)
+//Create Item
 router.post('/', uploadMiddleware.single('myfile'), createItem)
 
 module.exports = router;
